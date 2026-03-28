b0ebdc6c04c33d7215273d9fe53de6f5fed5f048
pacuito121 <corexmartex@gmail.com>
2026-03-28 03:58:00 -0600

    Add files via upload

---
+++ JS/catalogo.js
@@ -0,0 +1,263 @@
+let music = {
+    "Funk":{
+        "1":{
+            "Img":"../Img/Funk/EAW.jpg",
+            "Name":"Greatest hits",
+            "BName":"Earth Wind & Fire",
+            "MusicUrl":"../Audio/Funk/LovesHoliday.mp3"
+        },
+        "2":{
+            "Img":"../Img/Funk/MontagemDirecao.jpg",
+            "Name":"Montagem Direcao",
+            "BName":"DJ FKU",
+            "MusicUrl":"../Audio/Funk/MONTAGEMDIRECAO.mp3"
+        },
+        "3":{
+            "Img":"../Img/Funk/MontagemFavela.jpg",
+            "Name":"Montagem Favela",
+            "BName":"CHASHKAKEFIRA",
+            "MusicUrl":"../Audio/Funk/MONTAGEMFAVELA.mp3"
+        },
+        "4":{
+            "Img":"../Img/Funk/MontagemRugada.jpg",
+            "Name":"Montagem Rugada",
+            "BName":"JXNDRO",
+            "MusicUrl":"../Audio/Funk/MONTAGEMRUGADA.mp3"
+        },
+        "5":{
+            "Img":"../Img/Funk/MontagemXonada.jpg",
+            "Name":"Montagem Xonada",
+            "BName":"DJ Samir",
+            "MusicUrl":"../Audio/Funk/MONTAGEMXONADA.mp3"
+        },
+        "6":{
+            "Img":"../Img/Funk/NoBatidao.jpg",
+            "Name":"No Batidao",
+            "BName":"ZXKAI",
+            "MusicUrl":"../Audio/Funk/NOBATIDAO.mp3"
+        },
+        "7":{
+            "Img":"../Img/Funk/VoceNaMiraJumpStyle.jpg",
+            "Name":"Voce Na Mira JumpStyle",
+            "BName":"Sxilwix",
+            "MusicUrl":"../Audio/Funk/VOCENAMIRAJUMPSTYLE.mp3"
+        },
+        "8":{
+            "Img":"../Img/Funk/Vulfpeck.jpg",
+            "Name":"Clarity of Cal",
+            "BName":"Vulfpeck",
+            "MusicUrl":"../Audio/Funk/BigDipper.mp3"
+        },
+    },
+    "Rock en español":{
+        "1":{
+            "Img":"../Img/RockEspañol/Re.jpg",
+            "Name":"Re",
+            "BName":"Cafe Tacvba",
+            "MusicUrl":"../Audio/RockEspañol/EsaNoche.mp3"
+        },
+        "2":{
+            "Img":"../Img/RockEspañol/SenderosDeTraicion.jpg",
+            "Name":"Senderos de traición",
+            "BName":"Héroes del silencio",
+            "MusicUrl":"../Audio/RockEspañol/MalditoDuende.mp3"
+        },
+        "3":{
+            "Img":"../Img/RockEspañol/BigBang.jpg",
+            "Name":"Big Bang",
+            "BName":"Enanitos verdes",
+            "MusicUrl":"../Audio/RockEspañol/MiPrimerDiaSinTi.mp3"
+        },
+        "4":{
+            "Img":"../Img/RockEspañol/DondeJugaranLasNinas.jpg",
+            "Name":"¿Dónde jugarán las niñas?",
+            "BName":"Molotov",
+            "MusicUrl":"../Audio/RockEspañol/GimmeThaPower.mp3"
+        },
+        "5":{
+            "Img":"../Img/RockEspañol/ElSilencio.jpg",
+            "Name":"El silencio",
+            "BName":"Caifanes",
+            "MusicUrl":"../Audio/RockEspañol/NoDejesQue.mp3"
+        },
+        "6":{
+            "Img":"../Img/RockEspañol/Poetics.jpg",
+            "Name":"Poetics",
+            "BName":"PXNDX",
+            "MusicUrl":"../Audio/RockEspañol/SoloATerceros.mp3"
+        },
+        "7":{
+            "Img":"../Img/RockEspañol/Porfiado.jpg",
+            "Name":"Porfiado",
+            "BName":"Cuarteto de nos",
+            "MusicUrl":"../Audio/RockEspañol/LoMaloDeSerBueno.mp3"
+        },
+        "8":{
+            "Img":"../Img/RockEspañol/HombreSintetizador.jpg",
+            "Name":"Hombre sintetizador",
+            "BName":"Zurdok",
+            "MusicUrl":"../Audio/RockEspañol/AbreLosOjos.mp3"
+        },
+    },
+    "Metal":{
+        "1":{
+            "Img":"../Img/Metal/RideTheLighting.jpg",
+            "Name":"Ride the lighting",
+            "BName":"Metallica",
+            "MusicUrl":"../Audio/Metal/FadeToBlack.mp3"
+        },
+        "2":{
+            "Img":"../Img/Metal/FearOfTheDark.jpg",
+            "Name":"Fear of the dark",
+            "BName":"Iron Maiden",
+            "MusicUrl":"../Audio/Metal/FearOfTheDark.mp3"
+        },
+        "3":{
+            "Img":"../Img/Metal/MagosEspadasRosas.jpg",
+            "Name":"Magos espadas y rosas",
+            "BName":"Rata blanca",
+            "MusicUrl":"../Audio/Metal/HadaYMago.mp3"
+        },
+        "4":{
+            "Img":"../Img/Metal/DefendersOfTheFaith.jpg",
+            "Name":"Defenders of the faith",
+            "BName":"Judas Priest",
+            "MusicUrl":"../Audio/Metal/RockHardRideFree.mp3"
+        },
+        "5":{
+            "Img":"../Img/Metal/FidingGodBeforeGodFindsMe.jpg",
+            "Name":"Finding god before god finds me",
+            "BName":"Bad omens",
+            "MusicUrl":"../Audio/Metal/KingdomOfCards.mp3"
+        },
+        "6":{
+            "Img":"../Img/Metal/TheUltimateSin.jpg",
+            "Name":"The ultimate sin",
+            "BName":"Ozzy Ozbourne",
+            "MusicUrl":"../Audio/Metal/NeverKnowWhy.mp3"
+        },
+        "7":{
+            "Img":"../Img/Metal/TheEnd.jpg",
+            "Name":"The end",
+            "BName":"Black Sabbath",
+            "MusicUrl":"../Audio/Metal/TakeMeHome.mp3"
+        },
+        "8":{
+            "Img":"../Img/Metal/Repentless.jpg",
+            "Name":"Repentless",
+            "BName":"Slayer",
+            "MusicUrl":"../Audio/Metal/DelusionsOfSaviour.mp3"
+        },
+    },
+    "Nu Metal":{
+        "1":{
+            "Img":"../Img/NuMetal/Fallen.png",
+            "Name":"Fallen",
+            "BName":"Evanescence",
+            "MusicUrl":"../Audio/NuMetal/TakingOverMe.mp3"
+        },
+        "2":{
+            "Img":"../Img/NuMetal/Hipnotize.jpg",
+            "Name":"Hipnotize",
+            "BName":"System of a down",
+            "MusicUrl":"../Audio/NuMetal/Tentative.mp3"
+        },
+        "3":{
+            "Img":"../Img/NuMetal/Immortalized.jpg",
+            "Name":"Immortalized",
+            "BName":"Disturbed",
+            "MusicUrl":"../Audio/NuMetal/TheSoundOfSilence.mp3"
+        },
+        "4":{
+            "Img":"../Img/NuMetal/Issues.jpg",
+            "Name":"Issues",
+            "BName":"Korn",
+            "MusicUrl":"../Audio/NuMetal/FallingAwayFromMe.mp3"
+        },
+        "5":{
+            "Img":"../Img/NuMetal/Meteora.jpg",
+            "Name":"Meteora",
+            "BName":"Linkin Park",
+            "MusicUrl":"../Audio/NuMetal/SomewhereIBelong.mp3"
+        },
+        "6":{
+            "Img":"../Img/NuMetal/OneX.jpg",
+            "Name":"One X",
+            "BName":"Three days grace",
+            "MusicUrl":"../Audio/NuMetal/NeverTooLate.mp3"
+        },
+        "7":{
+            "Img":"../Img/NuMetal/Skindred.jpg",
+            "Name":"Skindred",
+            "BName":"Skindred",
+            "MusicUrl":"../Audio/NuMetal/Nobody.mp3"
+        },
+        "8":{
+            "Img":"../Img/NuMetal/TheGrayChapter.jpg",
+            "Name":"TheGrayChapter",
+            "BName":"Slipknot",
+            "MusicUrl":"../Audio/NuMetal/Goodbye.mp3"
+        },
+    }
+}
+let DJ = new Audio('../Audio/NoEraAmor.mp3')
+document.addEventListener('DOMContentLoaded',set("Funk"))
+function set(a){
+    let port = document.querySelectorAll(".item")
+    let ind = 1
+    port.forEach(Element=>{
+        Element.children.item(1).innerHTML = music[a][ind].Name
+        Element.children.item(2).innerHTML = music[a][ind].BName
+        ind++
+    })
+    ind = 1
+    let img = document.querySelectorAll(".tape")
+    img.forEach(Element=>{
+        Element.setAttribute("src",music[a][ind].Img)
+        ind++
+    })
+    ind = 1
+    let tape = document.querySelectorAll(".play")
+    tape.forEach(Element=>{
+        Element.id = music[a][ind].MusicUrl
+        ind++
+        Element.addEventListener("click",()=>{
+            DJ.pause()
+            DJ.src = Element.id
+            DJ.play()
+        })
+    })
+}
+let plat = document.getElementById("play") 
+plat.addEventListener("click",()=>{
+    DJ.play()
+})
+let paf = document.getElementById("pause")
+paf.addEventListener("click",()=>{
+    DJ.pause()
+})
+
+let opGen = document.querySelectorAll(".opGen");
+
+// 1. Primero, guardamos el texto limpio original de cada opción
+opGen.forEach(elemento => {
+    // Esto crea un atributo 'data-original' invisible en tu HTML
+    elemento.dataset.original = elemento.innerHTML; 
+});
+
+function clean() {
+
+    opGen.forEach(elemento => {
+        elemento.innerHTML = elemento.dataset.original;
+    });
+    console.log("Opciones limpiadas");
+}
+
+opGen.forEach(elemento => {
+    elemento.addEventListener("click", () => {
+        clean(); 
+
+        elemento.innerHTML = "<< " + elemento.dataset.original + " >>";
+
+    });
+});
---
+++ JS/comentarios.js
@@ -0,0 +1,326 @@
+let infus = {
+    "Funk":{
+        "1":{
+            "Img":"../Img/Funk/EAW.jpg",
+            "Name":"Greatest hits",
+            "BName":"Earth Wind & Fire",
+            "MusicUrl":"../Audio/Funk/LovesHoliday.mp3"
+        },
+        "2":{
+            "Img":"../Img/Funk/MontagemDirecao.jpg",
+            "Name":"Montagem Direcao",
+            "BName":"DJ FKU",
+            "MusicUrl":"../Audio/Funk/MONTAGEMDIRECAO.mp3"
+        },
+        "3":{
+            "Img":"../Img/Funk/MontagemFavela.jpg",
+            "Name":"Montagem Favela",
+            "BName":"CHASHKAKEFIRA",
+            "MusicUrl":"../Audio/Funk/MONTAGEMFAVELA.mp3"
+        },
+        "4":{
+            "Img":"../Img/Funk/MontagemRugada.jpg",
+            "Name":"Montagem Rugada",
+            "BName":"JXNDRO",
+            "MusicUrl":"../Audio/Funk/MONTAGEMRUGADA.mp3"
+        },
+        "5":{
+            "Img":"../Img/Funk/MontagemXonada.jpg",
+            "Name":"Montagem Xonada",
+            "BName":"DJ Samir",
+            "MusicUrl":"../Audio/Funk/MONTAGEMXONADA.mp3"
+        },
+        "6":{
+            "Img":"../Img/Funk/NoBatidao.jpg",
+            "Name":"No Batidao",
+            "BName":"ZXKAI",
+            "MusicUrl":"../Audio/Funk/NOBATIDAO.mp3"
+        },
+        "7":{
+            "Img":"../Img/Funk/VoceNaMiraJumpStyle.jpg",
+            "Name":"Voce Na Mira JumpStyle",
+            "BName":"Sxilwix",
+            "MusicUrl":"../Audio/Funk/VOCENAMIRAJUMPSTYLE.mp3"
+        },
+        "8":{
+            "Img":"../Img/Funk/Vulfpeck.jpg",
+            "Name":"Clarity of Cal",
+            "BName":"Vulfpeck",
+            "MusicUrl":"../Audio/Funk/BigDipper.mp3"
+        },
+    },
+    "Rock en español":{
+        "1":{
+            "Img":"../Img/RockEspañol/Re.jpg",
+            "Name":"Re",
+            "BName":"Cafe Tacvba",
+            "MusicUrl":"../Audio/RockEspañol/EsaNoche.mp3"
+        },
+        "2":{
+            "Img":"../Img/RockEspañol/SenderosDeTraicion.jpg",
+            "Name":"Senderos de traición",
+            "BName":"Héroes del silencio",
+            "MusicUrl":"../Audio/RockEspañol/MalditoDuende.mp3"
+        },
+        "3":{
+            "Img":"../Img/RockEspañol/BigBang.jpg",
+            "Name":"Big Bang",
+            "BName":"Enanitos verdes",
+            "MusicUrl":"../Audio/RockEspañol/MiPrimerDiaSinTi.mp3"
+        },
+        "4":{
+            "Img":"../Img/RockEspañol/DondeJugaranLasNinas.jpg",
+            "Name":"¿Dónde jugarán las niñas?",
+            "BName":"Molotov",
+            "MusicUrl":"../Audio/RockEspañol/GimmeThaPower.mp3"
+        },
+        "5":{
+            "Img":"../Img/RockEspañol/ElSilencio.jpg",
+            "Name":"El silencio",
+            "BName":"Caifanes",
+            "MusicUrl":"../Audio/RockEspañol/NoDejesQue.mp3"
+        },
+        "6":{
+            "Img":"../Img/RockEspañol/Poetics.jpg",
+            "Name":"Poetics",
+            "BName":"PXNDX",
+            "MusicUrl":"../Audio/RockEspañol/SoloATerceros.mp3"
+        },
+        "7":{
+            "Img":"../Img/RockEspañol/Porfiado.jpg",
+            "Name":"Porfiado",
+            "BName":"Cuarteto de nos",
+            "MusicUrl":"../Audio/RockEspañol/LoMaloDeSerBueno.mp3"
+        },
+        "8":{
+            "Img":"../Img/RockEspañol/HombreSintetizador.jpg",
+            "Name":"Hombre sintetizador",
+            "BName":"Zurdok",
+            "MusicUrl":"../Audio/RockEspañol/AbreLosOjos.mp3"
+        },
+    },
+    "Metal":{
+        "1":{
+            "Img":"../Img/Metal/RideTheLighting.jpg",
+            "Name":"Ride the lighting",
+            "BName":"Metallica",
+            "MusicUrl":"../Audio/Metal/FadeToBlack.mp3"
+        },
+        "2":{
+            "Img":"../Img/Metal/FearOfTheDark.jpg",
+            "Name":"Fear of the dark",
+            "BName":"Iron Maiden",
+            "MusicUrl":"../Audio/Metal/FearOfTheDark.mp3"
+        },
+        "3":{
+            "Img":"../Img/Metal/MagosEspadasRosas.jpg",
+            "Name":"Magos espadas y rosas",
+            "BName":"Rata blanca",
+            "MusicUrl":"../Audio/Metal/HadaYMago.mp3"
+        },
+        "4":{
+            "Img":"../Img/Metal/DefendersOfTheFaith.jpg",
+            "Name":"Defenders of the faith",
+            "BName":"Judas Priest",
+            "MusicUrl":"../Audio/Metal/RockHardRideFree.mp3"
+        },
+        "5":{
+            "Img":"../Img/Metal/FidingGodBeforeGodFindsMe.jpg",
+            "Name":"Finding god before god finds me",
+            "BName":"Bad omens",
+            "MusicUrl":"../Audio/Metal/KingdomOfCards.mp3"
+        },
+        "6":{
+            "Img":"../Img/Metal/TheUltimateSin.jpg",
+            "Name":"The ultimate sin",
+            "BName":"Ozzy Ozbourne",
+            "MusicUrl":"../Audio/Metal/NeverKnowWhy.mp3"
+        },
+        "7":{
+            "Img":"../Img/Metal/TheEnd.jpg",
+            "Name":"The end",
+            "BName":"Black Sabbath",
+            "MusicUrl":"../Audio/Metal/TakeMeHome.mp3"
+        },
+        "8":{
+            "Img":"../Img/Metal/Repentless.jpg",
+            "Name":"Repentless",
+            "BName":"Slayer",
+            "MusicUrl":"../Audio/Metal/DelusionsOfSaviour.mp3"
+        },
+    },
+    "Nu Metal":{
+        "1":{
+            "Img":"../Img/NuMetal/Fallen.png",
+            "Name":"Fallen",
+            "BName":"Evanescence",
+            "MusicUrl":"../Audio/NuMetal/TakingOverMe.mp3"
+        },
+        "2":{
+            "Img":"../Img/NuMetal/Hipnotize.jpg",
+            "Name":"Hipnotize",
+            "BName":"System of a down",
+            "MusicUrl":"../Audio/NuMetal/Tentative.mp3"
+        },
+        "3":{
+            "Img":"../Img/NuMetal/Immortalized.jpg",
+            "Name":"Immortalized",
+            "BName":"Disturbed",
+            "MusicUrl":"../Audio/NuMetal/TheSoundOfSilence.mp3"
+        },
+        "4":{
+            "Img":"../Img/NuMetal/Issues.jpg",
+            "Name":"Issues",
+            "BName":"Korn",
+            "MusicUrl":"../Audio/NuMetal/FallingAwayFromMe.mp3"
+        },
+        "5":{
+            "Img":"../Img/NuMetal/Meteora.jpg",
+            "Name":"Meteora",
+            "BName":"Linkin Park",
+            "MusicUrl":"../Audio/NuMetal/SomewhereIBelong.mp3"
+        },
+        "6":{
+            "Img":"../Img/NuMetal/OneX.jpg",
+            "Name":"One X",
+            "BName":"Three days grace",
+            "MusicUrl":"../Audio/NuMetal/NeverTooLate.mp3"
+        },
+        "7":{
+            "Img":"../Img/NuMetal/Skindred.jpg",
+            "Name":"Skindred",
+            "BName":"Skindred",
+            "MusicUrl":"../Audio/NuMetal/Nobody.mp3"
+        },
+        "8":{
+            "Img":"../Img/NuMetal/TheGrayChapter.jpg",
+            "Name":"TheGrayChapter",
+            "BName":"Slipknot",
+            "MusicUrl":"../Audio/NuMetal/Goodbye.mp3"
+        },
+    }
+}
+let calif = document.getElementById("Calif")
+calif.addEventListener("input",()=>{
+    document.getElementById("MSG").innerHTML = `Calificacion (${calif.value})`
+    console.log(calif.value)
+}
+)
+var Dj = new Audio("../")
+let ind = 1
+function set(a){
+    let img = document.querySelectorAll(".tip")
+    
+    img.forEach(Element=>{
+        Element.setAttribute("src",infus[a][ind].Img)
+        Element.setAttribute("alt",a)
+        Element.id = ind
+        ind++
+        Element.onclick = ()=>{
+            document.getElementById("sexBand").innerHTML  = infus[Element.getAttribute("alt")][Element.id].Name
+            document.getElementById("Portad").setAttribute("src",infus[Element.getAttribute("alt")][Element.id].Img)
+            Dj.src = infus[Element.getAttribute("alt")][Element.id].MusicUrl
+            document.getElementById("datax").children.item(0).innerHTML = infus[Element.getAttribute("alt")][Element.id].Name
+            document.getElementById("datax").children.item(1).innerHTML = infus[Element.getAttribute("alt")][Element.id].BName
+            Dj.pause()
+            document.getElementById("play").setAttribute("src","../Img/Play.png")
+            cargarComentariosYEstrellas(infus[Element.getAttribute("alt")][id].Name);
+        }
+    })
+    ind = 1
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    set("Rock en español")
+})
+let dura = Dj.duration
+let barra = document.getElementById("progress")
+    let play = document.getElementById("play")
+    play.addEventListener("click",()=>{
+        console.log(Dj.currentTime)
+        Dj.play()
+        play.setAttribute("src","../Img/Pause.png")
+    })
+    play.addEventListener("dblclick",()=>{
+        Dj.pause()
+        play.setAttribute("src","../Img/Play.png")
+    })
+setInterval(() => {
+    dura = Dj.duration
+    console.clear()
+    console.log("En total dura ",dura)
+    console.log("Justo ahora lleva ",Dj.currentTime)
+    barra.style.width = (Dj.currentTime/dura)*100 + "%"
+}, 100)
+// --- LÓGICA DE COMENTARIOS Y ESTRELLAS ---
+const formObj = document.querySelector("form");
+
+// Guardar comentario
+formObj.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let discoActual = document.getElementById("sexBand").innerHTML;
+    if (discoActual === "Selecciona un album") {
+        return alert("Por favor, selecciona un disco de la lista primero.");
+    }
+
+    let nuevoComentario = {
+        disco: discoActual,
+        usuario: document.getElementById("mane").value,
+        calificacion: parseInt(document.getElementById("Calif").value),
+        titular: document.getElementById("Titular").value,
+        texto: document.getElementById("comenta").value.trim()
+    };
+
+    let comentariosGuardados = JSON.parse(localStorage.getItem("ComentariosAdmirable")) || [];
+    comentariosGuardados.push(nuevoComentario);
+    localStorage.setItem("ComentariosAdmirable", JSON.stringify(comentariosGuardados));
+
+    formObj.reset();
+    document.getElementById("MSG").innerHTML = "Calificacion (5)";
+    cargarComentariosYEstrellas(discoActual);
+});
+
+// Calcular promedio y mostrar lista
+function cargarComentariosYEstrellas(nombreDisco) {
+    let comentariosGuardados = JSON.parse(localStorage.getItem("ComentariosAdmirable")) || [];
+    let comentariosDisco = comentariosGuardados.filter(c => c.disco === nombreDisco);
+    
+    let contenedorEstrellas = document.getElementById("stars");
+    let contenedorLista = document.getElementById("lista-comentarios");
+
+    // Si aún no has creado el id="lista-comentarios" en tu HTML, lo creamos dinámicamente
+    if (!contenedorLista) {
+        contenedorLista = document.createElement("article");
+        contenedorLista.id = "lista-comentarios";
+        document.querySelector("main").appendChild(contenedorLista);
+    }
+
+    if (comentariosDisco.length === 0) {
+        contenedorEstrellas.innerHTML = "Sin calificaciones aún";
+        contenedorLista.innerHTML = "<h3 align='center'>Aún no hay comentarios para este disco. ¡Sé el primero!</h3>";
+        return;
+    }
+
+    // Promedio de 5 estrellas
+    let suma = comentariosDisco.reduce((acc, comentario) => acc + comentario.calificacion, 0);
+    let promedioBase10 = suma / comentariosDisco.length;
+    let promedioBase5 = Math.round(promedioBase10 / 2);
+
+    let estrellasHTML = "";
+    for (let i = 1; i <= 5; i++) {
+        estrellasHTML += i <= promedioBase5 ? "⭐" : "☆";
+    }
+    contenedorEstrellas.innerHTML = `${estrellasHTML} <small>(${promedioBase10.toFixed(1)}/10)</small>`;
+
+    // Imprimir comentarios
+    contenedorLista.innerHTML = "<h3 style='margin-left: 20px;'>Comentarios de la comunidad:</h3>";
+    comentariosDisco.forEach(c => {
+        contenedorLista.innerHTML += `
+            <div style="border: 1px solid black; margin: 10px 20px; padding: 15px; background-color: #f9f9f9;">
+                <h4 style="margin: 0;">${c.titular} - ${c.calificacion}/10</h4>
+                <small>Por: <strong>${c.usuario}</strong></small>
+                <p style="margin-top: 10px;">${c.texto}</p>
+            </div>
+        `;
+    });
+}
